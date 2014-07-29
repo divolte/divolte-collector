348aa49a910d9d6fc762067ab2f6616d14290dfd
Friso van Vollenhoven <f.van.vollenhoven@gmail.com>
2014-07-29 10:41:40 +0200

    Minor source fixes.

--- src/main/java/io/divolte/server/IncomingRequestProcessingPool.java
+++ src/main/java/io/divolte/server/IncomingRequestProcessingPool.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
@@ -19,19 +18,18 @@ import com.typesafe.config.ConfigFactory;
 final class IncomingRequestProcessingPool {
     private final static Logger logger = LoggerFactory.getLogger(IncomingRequestProcessingPool.class);
     private final List<IncomingRequestProcessor> processors;
-    
+
     public IncomingRequestProcessingPool() {
         this(ConfigFactory.load());
     }
-    
+
     public IncomingRequestProcessingPool(Config config) {
         final int numSerializationThreads = config.getInt("divolte.server.incomingrequestprocessor.threads");
-        
+
         processors = new ArrayList<IncomingRequestProcessor>(numSerializationThreads);
-        Stream.generate(() -> new LinkedBlockingQueue<HttpServerExchange>())
+        Stream.generate(() -> new IncomingRequestProcessor())
         .limit(numSerializationThreads)
-        .forEach((queue) -> {
-            final IncomingRequestProcessor processor = new IncomingRequestProcessor();
+        .forEach((processor) -> {
             processors.add(processor);
             scheduleQueueReader(
                     Executors.newFixedThreadPool(
@@ -40,22 +38,22 @@ final class IncomingRequestProcessingPool {
                     processor);
         });
     }
-    
-    private void scheduleQueueReader(final ExecutorService es, final IncomingRequestProcessor queueReader) {
+
+    private void scheduleQueueReader(final ExecutorService es, final IncomingRequestProcessor processor) {
         CompletableFuture
-        .runAsync(() -> { queueReader.readQueue(); }, es)
+        .runAsync(processor::readQueue, es)
         .whenComplete((voidValue, error) -> {
             // In case the reader for some reason escapes its loop with an exception,
             // log any uncaught exceptions and reschedule
             if (error != null) {
                 logger.warn("Uncaught exception in incoming queue reader thread.", error);
-                scheduleQueueReader(es, queueReader);
+                scheduleQueueReader(es, processor);
             }
         });
     }
-    
+
     public void enqueueIncomingExchangeForProcessing(String partyId, HttpServerExchange exchange) {
-        // we assign request with the same party ID to the same thread,
+        // we assign requests with the same party ID to the same thread,
         // such that we do not re-order messages for the same party ID.
         processors.get((partyId.hashCode() & Integer.MAX_VALUE) % processors.size()).add(exchange);
     }
