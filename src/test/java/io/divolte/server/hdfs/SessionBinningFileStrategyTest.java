ece1b9534ea29edef150aa0b9f3c298122688c6e
Friso van Vollenhoven <f.van.vollenhoven@gmail.com>
2014-08-27 10:00:30 +0200

    Removed useless code from skeleton test.

--- src/test/java/io/divolte/server/hdfs/SessionBinningFileStrategyTest.java
+++ src/test/java/io/divolte/server/hdfs/SessionBinningFileStrategyTest.java
@@ -1,7 +1,4 @@
 package io.divolte.server.hdfs;
 
 public class SessionBinningFileStrategyTest {
-    public static void main(String[] args) {
-        System.out.println("20140821-01".compareTo("20140821-02"));
-    }
 }
