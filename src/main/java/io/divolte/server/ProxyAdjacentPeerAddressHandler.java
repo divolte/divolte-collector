bc74d93ee2434ba4dba85b507e3980fcbe9a6e2e
Friso van Vollenhoven <f.van.vollenhoven@gmail.com>
2014-10-02 16:50:52 +0200

    Optimize multiple headers flow.

--- src/main/java/io/divolte/server/ProxyAdjacentPeerAddressHandler.java
+++ src/main/java/io/divolte/server/ProxyAdjacentPeerAddressHandler.java
@@ -11,20 +11,9 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Joiner;
-
-/**
- * Handler that sets the peer address to the value of the X-Forwarded-For header.
- * <p/>
- * This should only be used behind a proxy that always sets this header, otherwise it
- * is possible for an attacker to forge their peer address;
- *
- * @author Stuart Douglas
- */
 public class ProxyAdjacentPeerAddressHandler implements HttpHandler {
     private static final Logger logger = LoggerFactory.getLogger(ProxyAdjacentPeerAddressHandler.class);
 
-    private final static Joiner joiner = Joiner.on(',');
     private final HttpHandler next;
 
     public ProxyAdjacentPeerAddressHandler(HttpHandler next) {
@@ -34,8 +23,7 @@ public class ProxyAdjacentPeerAddressHandler implements HttpHandler {
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
         Optional
-        .ofNullable(exchange.getRequestHeaders().get(Headers.X_FORWARDED_FOR))
-        .map(joiner::join)
+        .ofNullable(exchange.getRequestHeaders().getLast(Headers.X_FORWARDED_FOR))
         .ifPresent((forwardedFor) -> {
             int index = forwardedFor.lastIndexOf(',');
             final String value;
