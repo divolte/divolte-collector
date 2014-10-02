2736221ac05d2071a99c265f9213fec6786b3337
Andrew Snare <andrewsnare@godatadriven.com>
2014-10-02 16:35:45 +0200

    Attach an ETag to the pixel responses. This lets us detect some circumstances under which a browser re-issues the pixel request.

--- src/main/java/io/divolte/server/BaseEventHandler.java
+++ src/main/java/io/divolte/server/BaseEventHandler.java
@@ -4,11 +4,13 @@ import io.divolte.server.CookieValues.CookieValue;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.AttachmentKey;
-import io.undertow.util.HeaderMap;
+import io.undertow.util.ETag;
+import io.undertow.util.ETagUtils;
 import io.undertow.util.Headers;
 import io.undertow.util.StatusCodes;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.util.Deque;
 import java.util.Objects;
@@ -16,10 +18,16 @@ import java.util.Optional;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Strings;
 import com.google.common.io.Resources;
 
 @ParametersAreNonnullByDefault
 public abstract class BaseEventHandler implements HttpHandler {
+    private static Logger logger = LoggerFactory.getLogger(BaseEventHandler.class);
+
     public static final AttachmentKey<CookieValue> PARTY_COOKIE_KEY = AttachmentKey.create(CookieValue.class);
     public static final AttachmentKey<CookieValue> SESSION_COOKIE_KEY = AttachmentKey.create(CookieValue.class);
     public static final AttachmentKey<String> PAGE_VIEW_ID_KEY = AttachmentKey.create(String.class);
@@ -44,6 +52,8 @@ public abstract class BaseEventHandler implements HttpHandler {
     public final static String SCREEN_PIXEL_HEIGHT_QUERY_PARAM = "j";
     public final static String DEVICE_PIXEL_RATIO = "k";
 
+    private final static ETag SENTINEL_ETAG = new ETag(false, "6b3edc43-20ec-4078-bc47-e965dd76b88a");
+    private final static String SENTINEL_ETAG_VALUE = SENTINEL_ETAG.toString();
 
     private final ByteBuffer transparentImage;
     protected final IncomingRequestProcessingPool processingPool;
@@ -68,13 +78,47 @@ public abstract class BaseEventHandler implements HttpHandler {
          * no longer be available after the response has been sent. So we materialize it here
          * to ensure it's available further down the chain.
          */
-        exchange.setSourceAddress(exchange.getSourceAddress());
-        // Subclasses are responsible to logging events.
-        // We just ensure the pixel is always returned, no matter what.
-        try {
-            logEvent(exchange);
-        } finally {
-            serveImage(exchange);
+        final InetSocketAddress sourceAddress = exchange.getSourceAddress();
+        exchange.setSourceAddress(sourceAddress);
+
+        /*
+         * Set up the headers that we always send as a response, irrespective of what type it
+         * will be. Note that the client is responsible for ensuring that ensures that each request
+         * is unique.
+         * The cache-related headers are intended to prevent spurious reloads for an event.
+         * (Being a GET request, agents are free to re-issue the request at will. We don't want this.)
+         * As a last resort, we try to detect duplicates via the ETag header.
+         */
+        exchange.getResponseHeaders()
+                .put(Headers.CONTENT_TYPE, "image/gif")
+                .put(Headers.ETAG, SENTINEL_ETAG_VALUE)
+                .put(Headers.CACHE_CONTROL, "private, no-cache, proxy-revalidate")
+                .put(Headers.PRAGMA, "no-cache")
+                .put(Headers.EXPIRES, "Fri, 14 Apr 1995 11:30:00 GMT");
+
+        // If an ETag is present, this is a duplicate event.
+        if (ETagUtils.handleIfNoneMatch(exchange, SENTINEL_ETAG, true)) {
+            /*
+             * Subclasses are responsible to logging events.
+             * We just ensure the pixel is always returned, no matter what.
+             */
+            try {
+                logEvent(exchange);
+            } finally {
+                exchange.setResponseCode(StatusCodes.ACCEPTED);
+                exchange.getResponseSender().send(transparentImage.slice());
+            }
+        } else {
+            if (logger.isDebugEnabled()) {
+                final String queryString = exchange.getQueryString();
+                final String requestUrl = exchange.getRequestURL();
+                final String fullUrl = Strings.isNullOrEmpty(queryString)
+                                       ? requestUrl
+                                       : requestUrl + '?' + queryString;
+                logger.debug("Ignoring duplicate event from {}: {}", sourceAddress, fullUrl);
+            }
+            exchange.setResponseCode(StatusCodes.NOT_MODIFIED);
+            exchange.endExchange();
         }
     }
 
@@ -89,22 +133,6 @@ public abstract class BaseEventHandler implements HttpHandler {
      * @param exchange the HTTP exchange from which event data can be extracted.
      */
     protected abstract void logEvent(final HttpServerExchange exchange);
-
-    private void serveImage(final HttpServerExchange exchange) {
-        exchange.setResponseCode(StatusCodes.ACCEPTED);
-
-        final HeaderMap responseHeaders = exchange.getResponseHeaders();
-        // The client ensures that each request is unique.
-        // The cache-related headers are intended to prevent spurious reloads.
-        // (As a GET request, agents are free to re-issue the request at will. This disturbs us.)
-        responseHeaders
-            .put(Headers.CONTENT_TYPE, "image/gif")
-            .put(Headers.CACHE_CONTROL, "private, no-cache, proxy-revalidate")
-            .put(Headers.PRAGMA, "no-cache")
-            .put(Headers.EXPIRES, "Fri, 14 Apr 1995 11:30:00 GMT");
-
-        exchange.getResponseSender().send(transparentImage.slice());
-    }
 
     protected static class IncompleteRequestException extends Exception {
         private static final long serialVersionUID = 1L;
