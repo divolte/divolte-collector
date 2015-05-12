3c5c6725f128b0e06841be05317bce8ecfffef67
Andrew Snare <andrewsnare@godatadriven.com>
2015-05-12 11:44:23 +0200

    Some more tests for the mincode parser.

--- src/test/java/io/divolte/server/mincode/MincodeParserTest.java
+++ src/test/java/io/divolte/server/mincode/MincodeParserTest.java
@@ -20,6 +20,7 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.io.CharStreams;
 import com.google.common.io.Resources;
 import org.hamcrest.Matchers;
 import org.junit.Rule;
@@ -28,8 +29,12 @@ import org.junit.rules.ExpectedException;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.math.BigInteger;
 import java.util.Base64;
+import java.util.Random;
 
 import static org.junit.Assert.*;
 
@@ -136,6 +141,30 @@ public class MincodeParserTest {
         assertEquals(0, parser.getTextLength());
     }
 
+    // None of these need to be escaped.
+    private static char[] SAFE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+
+    private static String createLargeStringValue() {
+        final int length = 3 * JACKSON_BUFFER_SIZE + 1;
+        final StringBuilder sb = new StringBuilder(length);
+        final Random random = new Random(0);
+        for (int i = 0; i < length; ++i) {
+            sb.append(SAFE_CHARS[random.nextInt(SAFE_CHARS.length)]);
+        }
+        return sb.toString();
+    }
+
+    @Test
+    public void testLongLargeStringValueDecoding() throws IOException {
+        // Build a very large string value to decode.
+        final String value = createLargeStringValue();
+        final String record = 's' + value + '!';
+
+        // Using a reader ensures the parser performs its own buffering instead
+        // of just scanning directly from the string.
+        assertEquals(value, MAPPER.readValue(new StringReader(record), String.class));
+    }
+
     @Test
     public void testFirstRecordCannotBeEndOfObjectOrArray() throws IOException {
         expectedException.expect(JsonParseException.class);
@@ -178,5 +207,109 @@ public class MincodeParserTest {
         expectedException.expectMessage("Invalid number record");
         expectedException.expectCause(Matchers.isA(NumberFormatException.class));
         createParser("j54@@!").nextToken();
+    }
+
+    @Test
+    public void testFloatingPointIntegerRecord() throws IOException {
+        // Floating point records can also supply integers. In this case
+        // we should supply the correct token type.
+        final JsonParser parser = createParser("j1234!");
+        assertEquals(JsonToken.VALUE_NUMBER_INT, parser.nextToken());
+        assertEquals(1234, parser.getValueAsInt());
+    }
+
+    // Per JACKSON-804 the max for a byte is 255.
+    private static final BigInteger MAX_BYTE = BigInteger.valueOf(255);
+    private static final BigInteger MAX_SHORT = BigInteger.valueOf(Short.MAX_VALUE);
+    private static final BigInteger MAX_INT = BigInteger.valueOf(Integer.MAX_VALUE);
+    private static final BigInteger MAX_LONG = BigInteger.valueOf(Long.MAX_VALUE);
+
+    private static <T extends Number> void assertInRange(final BigInteger value,
+                                                         final Class<T> type) throws IOException {
+        final T parsedValue = MAPPER.readValue("j" + value + '!', type);
+        assertEquals(value, MAPPER.convertValue(parsedValue, BigInteger.class));
+    }
+
+    private static <T extends Number> void assertInRange(final BigInteger expectedValue,
+                                                         final BigInteger value,
+                                                         final Class<T> type) throws IOException {
+        final T parsedValue = MAPPER.readValue("j" + value + '!', type);
+        assertEquals(expectedValue, MAPPER.convertValue(parsedValue.longValue(), BigInteger.class));
+    }
+
+    private <T extends Number> void assertOutOfRange(final String expectedMessageSubstring,
+                                                     final BigInteger maxValue,
+                                                     final Class<T> type) throws IOException {
+        expectedException.expect(JsonParseException.class);
+        expectedException.expectMessage(expectedMessageSubstring);
+        MAPPER.readValue("j" + maxValue.add(BigInteger.ONE) + '!', type);
+    }
+
+    @Test
+    public void testIntegerValueByteInRange() throws IOException {
+        assertInRange(BigInteger.valueOf(-1), MAX_BYTE, Byte.class);
+    }
+
+    @Test
+    public void testIntegerValueByteOutOfRange() throws IOException {
+        assertOutOfRange("out of range of Java byte", MAX_BYTE, Byte.class);
+    }
+
+    @Test
+    public void testIntegerValueShortInRange() throws IOException {
+        assertInRange(MAX_SHORT, Short.class);
+    }
+
+    @Test
+    public void testIntegerValueShortOutOfRange() throws IOException {
+        assertOutOfRange("out of range of Java short", MAX_SHORT, Short.class);
+    }
+
+    @Test
+    public void testIntegerValueIntegerInRange() throws IOException {
+        assertInRange(MAX_INT, Integer.class);
+    }
+
+    @Test
+    public void testIntegerValueIntegerOutOfRange() throws IOException {
+        assertOutOfRange("out of range of int", MAX_INT, Integer.class);
+    }
+
+    @Test
+    public void testIntegerValueLongInRange() throws IOException {
+        assertInRange(MAX_LONG, Long.class);
+    }
+
+    @Test
+    public void testIntegerValueLongOutOfRange() throws IOException {
+        assertOutOfRange("out of range of long", MAX_LONG, Long.class);
+    }
+
+    @Test
+    public void testIntegerValueBigIntegerInRange() throws IOException {
+        assertInRange(MAX_BYTE,                      BigInteger.class);
+        assertInRange(MAX_BYTE.add(BigInteger.ONE),  BigInteger.class);
+        assertInRange(MAX_SHORT,                     BigInteger.class);
+        assertInRange(MAX_SHORT.add(BigInteger.ONE), BigInteger.class);
+        assertInRange(MAX_INT,                       BigInteger.class);
+        assertInRange(MAX_INT.add(BigInteger.ONE),   BigInteger.class);
+        assertInRange(MAX_LONG,                      BigInteger.class);
+        assertInRange(MAX_LONG.add(BigInteger.ONE),  BigInteger.class);
+    }
+
+    @Test
+    public void testOnlyReadWhatIsRequired() throws IOException {
+        final Reader reader = new StringReader("sA string record!Extra trailing data.");
+        final JsonParser parser = MAPPER.getFactory().createParser(reader);
+        assertEquals("A string record", MAPPER.readValue(parser, String.class));
+
+        // Check the trailing data can be retrieved.
+        final StringWriter writer = new StringWriter();
+        parser.releaseBuffered(writer);
+        assertTrue(0 < writer.getBuffer().length());
+        CharStreams.copy(reader, writer);
+
+        // Check the trailer could be retrieved.
+        assertEquals("Extra trailing data.", writer.toString());
     }
 }
