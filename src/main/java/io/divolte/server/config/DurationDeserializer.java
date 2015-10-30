3b34668ca702a709523b089dccb7567ce4d8a229
Andrew Snare <andrewsnare@godatadriven.com>
2015-10-30 16:11:21 +0100

    Unused import.

--- src/main/java/io/divolte/server/config/DurationDeserializer.java
+++ src/main/java/io/divolte/server/config/DurationDeserializer.java
@@ -7,7 +7,6 @@ import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
