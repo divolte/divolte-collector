4f183fe954333950d544b1f56f16eb791b543df4
Andrew Snare <andrewsnare@godatadriven.com>
2015-12-31 11:25:48 +0100

    Reindent with 4 spaces, as with the rest of the project.

--- src/main/groovy/io/divolte/groovyscript/MappingBase.groovy
+++ src/main/groovy/io/divolte/groovyscript/MappingBase.groovy
@@ -22,89 +22,87 @@ import io.divolte.server.recordmapping.SchemaMappingException
 import java.net.URI
 
 abstract class MappingBase extends Script {
-  // The mapping property is set on the scripts binding by the
-  // initializing Java code. This is to avoid cyclic dependencies
-  // between Java and Groovy code, as that introduces issues with
-  // IDE support.
+    // The mapping property is set on the scripts binding by the
+    // initializing Java code. This is to avoid cyclic dependencies
+    // between Java and Groovy code, as that introduces issues with
+    // IDE support.
 
-  def mapping(Closure c) {
-    c.delegate = mapping
-    c.run()
-  }
+    def mapping(Closure c) {
+        c.delegate = mapping
+        c.run()
+    }
 
-  def map(literal) {
-    [
-      'onto': { String fieldName -> mapping.map(fieldName, literal) }
-    ]
-  }
+    def map(literal) {
+        [
+            'onto': { String fieldName -> mapping.map(fieldName, literal) }
+        ]
+    }
 
-  def map(ValueProducer producer) {
-    [
-      'onto': { String fieldName -> mapping.map(fieldName, producer) }
-    ]
-  }
+    def map(ValueProducer producer) {
+        [
+            'onto': { String fieldName -> mapping.map(fieldName, producer) }
+        ]
+    }
 
-  def map(Closure<ValueProducer> producer) {
-    map(producer.call())
-  }
+    def map(Closure<ValueProducer> producer) {
+        map(producer.call())
+    }
 
-  def when(ValueProducer<Boolean> producer) {
-    [
-      'apply': {
-        Closure closure ->
-        mapping.when(producer, closure)
-      },
-      'stop': { mapping.when(producer, { mapping.stop() }) },
-      'exit': { mapping.exitWhen(producer) }
-    ]
-  }
+    def when(ValueProducer<Boolean> producer) {
+        [
+            'apply': { Closure closure -> mapping.when(producer, closure) },
+            'stop': { mapping.when(producer, { mapping.stop() }) },
+            'exit': { mapping.exitWhen(producer) }
+        ]
+    }
 
-  def when(Closure<ValueProducer<Boolean>> producer) {
-    when(producer.call())
-  }
+    def when(Closure<ValueProducer<Boolean>> producer) {
+        when(producer.call())
+    }
 
-  def match(String regex) {
-    [
-      'against': { ValueProducer<String> producer -> mapping.matcher(producer, regex) }
-    ]
-  }
+    def match(String regex) {
+        [
+            'against': { ValueProducer<String> producer -> mapping.matcher(producer, regex) }
+        ]
+    }
 
-  static def not(ValueProducer<Boolean> producer) {
-    producer.negate()
-  }
+    static def not(ValueProducer<Boolean> producer) {
+        producer.negate()
+    }
 
-  def int32 = Integer.TYPE
-  def int64 = Long.TYPE
-  def fp32 = Float.TYPE
-  def fp64 = Double.TYPE
-  def bool = Boolean.TYPE
-  def uri = URI.class
+    def int32 = Integer.TYPE
+    def int64 = Long.TYPE
+    def fp32 = Float.TYPE
+    def fp64 = Double.TYPE
+    def bool = Boolean.TYPE
+    def uri = URI.class
 
-  def parse(ValueProducer<String> producer) {
-    [
-      'to': { t -> switch(t) {
-          case int32:
-            mapping.toInt(producer)
-            break
-          case int64:
-            mapping.toLong(producer)
-            break
-          case fp32:
-            mapping.toFloat(producer)
-            break
-          case fp64:
-            mapping.toDouble(producer)
-            break
-          case bool:
-            mapping.toBoolean(producer)
-            break
-          case uri:
-            mapping.parseUri(producer)
-            break
-          default:
-            throw new SchemaMappingException("Cannot parse string into type: %s", t)
-        }
-      }
-    ]
-  }
+    def parse(ValueProducer<String> producer) {
+        [
+            'to': {
+                t -> switch(t) {
+                    case int32:
+                        mapping.toInt(producer)
+                        break
+                    case int64:
+                        mapping.toLong(producer)
+                        break
+                    case fp32:
+                        mapping.toFloat(producer)
+                        break
+                    case fp64:
+                        mapping.toDouble(producer)
+                        break
+                    case bool:
+                        mapping.toBoolean(producer)
+                        break
+                    case uri:
+                        mapping.parseUri(producer)
+                        break
+                    default:
+                        throw new SchemaMappingException("Cannot parse string into type: %s", t)
+                }
+            }
+        ]
+    }
 }
