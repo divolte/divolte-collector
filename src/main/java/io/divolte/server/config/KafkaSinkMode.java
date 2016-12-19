99a13c9706583ba3653264ed80dd0ee6d808e912
fvlankvelt <frank@vanlankvelt.net>
2016-12-19 16:11:37 +0100

    use jackson's built-in support for enums

--- src/main/java/io/divolte/server/config/KafkaSinkMode.java
+++ src/main/java/io/divolte/server/config/KafkaSinkMode.java
@@ -16,26 +16,11 @@
 
 package io.divolte.server.config;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-
-import java.util.NoSuchElementException;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum KafkaSinkMode {
-    NAKED, CONFLUENT;
-
-    @JsonCreator
-    public static KafkaSinkMode forValue(String value) {
-        for (KafkaSinkMode mode : KafkaSinkMode.values()) {
-            if (mode.toString().equals(value)) {
-                return mode;
-            }
-        }
-        throw new NoSuchElementException("Kafka sink mode " + value + " is not supported");
-    }
-
-    @JsonValue
-    public String toString() {
-        return name().toLowerCase();
-    }
+    @JsonProperty("naked")
+    NAKED,
+    @JsonProperty("confluent")
+    CONFLUENT
 }
