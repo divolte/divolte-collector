b08c8052bbaac679e598865ee372349e63672bfb
Friso van Vollenhoven <f.van.vollenhoven@gmail.com>
2017-03-21 15:25:20 +0100

    Add bucket name configuration.

--- src/main/java/io/divolte/server/config/GoogleCloudStorageSinkConfiguration.java
+++ src/main/java/io/divolte/server/config/GoogleCloudStorageSinkConfiguration.java
@@ -6,6 +6,7 @@ import javax.annotation.ParametersAreNullableByDefault;
 import org.apache.avro.Schema;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.MoreObjects;
 
 import io.divolte.server.filesinks.FileFlushingPool;
@@ -14,15 +15,19 @@ import io.divolte.server.filesinks.gcs.GoogleCloudStorageFileManager;
 
 @ParametersAreNonnullByDefault
 public class GoogleCloudStorageSinkConfiguration extends FileSinkConfiguration {
+    public final String bucket;
+
     @JsonCreator
     @ParametersAreNullableByDefault
-    GoogleCloudStorageSinkConfiguration(final FileStrategyConfiguration fileStrategy) {
+    GoogleCloudStorageSinkConfiguration(final FileStrategyConfiguration fileStrategy, @JsonProperty(required = true) final String bucket) {
         super(fileStrategy);
+        this.bucket = bucket;
     }
 
     @Override
     protected MoreObjects.ToStringHelper toStringHelper() {
         return super.toStringHelper()
+            .add("bucket", bucket)
             .add("fileStrategy", fileStrategy);
     }
 
