4df252e00b3c9bba3aeecdcfdd75dd7f867423b8
Friso van Vollenhoven <f.van.vollenhoven@gmail.com>
2017-04-23 12:29:35 +0200

    Turn interface docs into proper JavaDoc.

--- src/main/java/io/divolte/server/filesinks/FileManager.java
+++ src/main/java/io/divolte/server/filesinks/FileManager.java
@@ -22,47 +22,60 @@ import javax.annotation.concurrent.NotThreadSafe;
 
 import io.divolte.server.AvroRecordBuffer;
 
-/*
+/**
  * Abstraction for file based sinks that allows to cater for different types of (remote) file
  * systems.
  */
 @NotThreadSafe
 public interface FileManager {
-    /*
+    /**
      * Create a new file in inflight status. Throwing IOException will cause the client to go into a
      * retry cycle, by discarding the current file(s) and attempting to open a new one every so
      * often.
+     *
+     * @param name Name of the file to be created, excluding directory.
+     * @return A {@link DivolteFile} instance.
+     * @throws IOException In case of errors or unavailability of the remote file system.
      */
     DivolteFile createFile(final String name) throws IOException;
 
     interface DivolteFile {
-        /*
+        /**
          * Append a single record to the file. May or may not buffer depending on implementation.
          * Throwing IOException will cause the client to go into a retry cycle, by discarding the
          * current file(s) and attempting to open a new one every so often.
+         *
+         * @param buffer The record to append to the open file.
+         * @throws IOException In case of errors or unavailability of the remote file system.
          */
         void append(AvroRecordBuffer buffer) throws IOException;
 
-        /*
+        /**
          * Close and publish the file. For most implementations this will imply a move / rename to a
          * different directory. Calling append(...) after this should throw IllegalStateException.
+         *
+         * @throws IOException In case of errors or unavailability of the remote file system.
          */
         void closeAndPublish() throws IOException;
 
-        /*
+        /**
          * Sync the underlying file.
+         *
+         * @throws IOException In case of errors or unavailability of the remote file system.
          */
         void sync() throws IOException;
 
-        /*
+        /**
          * Remove this file. Should close and attempt to delete it. Calling other methods
          * afterwards should throw IllegalStateException.
+         *
+         * @throws IOException In case of errors or unavailability of the remote file system.
          */
         void discard() throws IOException;
     }
 
     interface FileManagerFactory {
-        /*
+        /**
          * Attempt to use the loaded configuration to make a file system connection. Implementations
          * should verify that the configuration holds all necessary information to successfully
          * connect to the file system and can additionally verify matters like appropriate
@@ -72,11 +85,13 @@ public interface FileManager {
          */
         void verifyFileSystemConfiguration();
 
-        /*
+        /**
          * Create an instance of FileManager that connects to the remote file system. In the event
          * of unavailability of the remote file system, this method should still return a
          * FileManager as recovering the underlying connection is expected to happen as part of a
          * retry cycle.
+         *
+         * @return A {@link FileManager} instance.
          */
         FileManager create();
     }
