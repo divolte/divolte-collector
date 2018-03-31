cc58c9e1b6e0cadc6e111dce78801702fd3f0768
Andrew Snare <andrewsnare@godatadriven.com>
2018-03-31 20:48:19 +0200

    Refactor flaky test.
    This test had a race condition. It was passing during local development,
    but sometimes failing on our CI machine.

--- src/test/java/io/divolte/server/ServerPingTest.java
+++ src/test/java/io/divolte/server/ServerPingTest.java
@@ -17,6 +17,7 @@
 package io.divolte.server;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Stopwatch;
 import com.google.common.io.ByteStreams;
 import io.divolte.server.ServerTestUtils.TestServer;
 import org.junit.After;
@@ -29,10 +30,13 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Optional;
 
 import static java.net.HttpURLConnection.HTTP_OK;
 import static java.net.HttpURLConnection.HTTP_UNAVAILABLE;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 @ParametersAreNonnullByDefault
 public class ServerPingTest {
@@ -46,31 +50,61 @@ public class ServerPingTest {
     }
 
     @Test
-    public void shouldRespondToHealthCheck() throws IOException {
+    public void shouldRespondToHealthCheck() throws IOException, InterruptedException {
         Preconditions.checkState(null != testServer);
         final URL url = new URL(String.format("http://%s:%d/ping", testServer.host, testServer.port));
-        final HttpURLConnection conn1 = (HttpURLConnection) url.openConnection();
-        try {
-            conn1.setRequestMethod("GET");
-            assertEquals(HTTP_OK, conn1.getResponseCode());
-            assertEquals("text/plain; charset=utf-8", conn1.getContentType());
-            final String body = new String(ByteStreams.toByteArray(conn1.getInputStream()), StandardCharsets.UTF_8);
-            assertEquals("pong", body);
-        } finally {
-            conn1.disconnect();
-        }
 
-        // Start the shutdown procedure
+        // Check the server is up.
+        pingServer(url, Optional.of("pong"));
+
+        // Start the shutdown procedure.
         testServer.shutdown();
 
-        final HttpURLConnection conn2 = (HttpURLConnection) url.openConnection();
-        try {
-            conn2.setRequestMethod("GET");
-            assertEquals(HTTP_UNAVAILABLE, conn2.getResponseCode());
-            assertEquals("text/plain; charset=utf-8", conn2.getContentType());
-        } finally {
-            conn2.disconnect();
+        // The shutdown is async, and we're racing it here.
+        // As a result we need to poll until we see the 504. Any 200 codes in the interim can be ignored.
+        final Stopwatch timer = Stopwatch.createStarted();
+        final Duration timeout = Duration.ofSeconds(10);
+        loop:
+        for (;;) {
+            final int responseCode = pingServer(url, Optional.empty());
+            switch (responseCode) {
+                case HTTP_OK:
+                    // Not yet failure; check to see if too much time has elapsed.
+                    if (timer.elapsed().compareTo(timeout) >= 0) {
+                        // Took too long. Fail the test.
+                        fail(String.format("Server took too long to start shutting down; expected %d but after %s server ping was still responding with %d.",
+                                           HTTP_UNAVAILABLE, timeout, HTTP_OK));
+                    }
+                    // Wait a bit before trying again.
+                    Thread.sleep(Duration.ofMillis(100).toMillis());
+                    break;
+                case HTTP_UNAVAILABLE:
+                    // Test success.
+                    break loop;
+                default:
+                    fail(String.format("Unexpected response code from server: %d; expected %d or %d.",
+                        responseCode, HTTP_OK, HTTP_UNAVAILABLE));
+            }
         }
+    }
+
+    private static int pingServer(final URL url, final Optional<String> expectedBody) throws IOException {
+        final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        final int responseCode;
+        try {
+            conn.setRequestMethod("GET");
+            responseCode = conn.getResponseCode();
+            assertEquals("text/plain; charset=utf-8", conn.getContentType());
+            expectedBody.ifPresent(expected -> {
+                final String body =
+                    new String(IOExceptions.wrap(() -> ByteStreams.toByteArray(conn.getInputStream())).get(),
+                               StandardCharsets.UTF_8);
+                assertEquals(expected, body);
+            });
+        } finally {
+            conn.disconnect();
+        }
+        return responseCode;
     }
 
     @After
