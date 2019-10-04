3cedff9a1cc6f07b1ed5b4c5e9f434ae3223d074
Andrew Snare <andrewsnare@godatadriven.com>
2019-10-04 17:08:36 +0200

    Upgrade to the current version of maven.
    And use curl instead of wget.

--- config/ci/install-schemas.sh
+++ config/ci/install-schemas.sh
@@ -2,9 +2,10 @@
 
 echo "Install Apache Maven"
 
-wget -q -O - http://archive.apache.org/dist/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz | tar -xzf - -C /usr/local \
-      && ln -s /usr/local/apache-maven-3.3.3 /usr/local/apache-maven \
-      && ln -s /usr/local/apache-maven/bin/mvn /usr/local/bin/mvn
+curl -L https://archive.apache.org/dist/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.tar.gz |
+  tar -xzf - -C /usr/local &&
+  ln -s /usr/local/apache-maven-3.6.2 /usr/local/apache-maven &&
+  ln -s /usr/local/apache-maven/bin/mvn /usr/local/bin/
 
 echo "Install the Divolte schema"
 
